e559a5218a118b7e9ed88d707d7d8ce7a42e8c0a
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-01 17:00:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,10 @@
 #include "lib.h"
-int n_primo(int a, int b) {
+int primo(int a, int b) {
     bool x=true;
     if (b!=1) {
         if ((a % b) != 0) {
             b--;
-            x = n_primo(a, b);
+            x = primo(a, b);
         } else {
             x = false;
         }
