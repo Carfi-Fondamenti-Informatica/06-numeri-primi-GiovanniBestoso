d669290ff8f5b3fdb2648b65fa330129fdc30761
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-01 16:17:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+  int n=0, i=2;
+        cin>>n;
+if(primo(n,i)== true){
+    cout<<"numero primo"<<endl;
+}else if ((primo(n,i)==false)){
+    cout<<"numero non primo"<<endl;
+}
   return 0;
 }
