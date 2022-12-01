011306cc0f3834403c1e06edbbaebb5da206fa7f
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-01 16:59:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+  int main() {
+int a=0;
+cin >> a;
+int b=(a-1);
+if (primo(a,b)) {
+    cout << "numero primo";
+}else if (primo(a,b)==false) {
+    cout << "numero non primo";
+} return 0;
+}
