bc1af0ede470185552ec18a9409dad503580b534
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-01 16:58:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,1 @@
 
-int primo(int n, int i) {
-  bool x=true;
-  
-  if (i!=1) {
-    if ((n % i) != 0) {
-      m--;
-      x=primo(n, i);
-    } else {
-      x=false;
-    }
-  } else {
-    x=true;
-  }
-  return x;
-}
